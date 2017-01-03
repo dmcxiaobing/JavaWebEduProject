e628e7c56f7e55b16ec3899f01fb52fca85e6ddf
qq986945193 <986945193@qq.com>
2017-01-03 14:03:48 +0800

    更新url

--- JavaWebBase/src/com/qq986945193/javaweb/constans/Urls.java
+++ JavaWebBase/src/com/qq986945193/javaweb/constans/Urls.java
@@ -1,4 +1,5 @@
 package com.qq986945193.javaweb.constans;
+
 /**
  * @Author ：程序员小冰
  * @新浪微博 ：http://weibo.com/mcxiaobing
@@ -7,5 +8,22 @@ package com.qq986945193.javaweb.constans;
  * @OsChina空间: https://my.oschina.net/mcxiaobing
  */
 public class Urls {
-
+	/* 程序员小冰CSDN博客 */
+	public static final String CSDN_BLOG_DAVID = "http://blog.csdn.net/qq_21376985";
+	/* 程序员小冰新浪微博 */
+	public static final String SINA_WEIBO = "http://weibo.com/mcxiaobing";
+	/* 程序员小冰GitHub主页 */
+	public static final String GITHUB_DAVID = "https://github.com/QQ986945193";
+	/* 程序员小冰OSChina的Git主页 */
+	public static final String GIT_OSCHINA_DAVID = "http://git.oschina.net/MCXIAOBING";
+	/* 程序员小冰CSDN空间 */
+	public static final String CSDN_DAVID = "http://my.csdn.net/qq_21376985";
+	/* 程序员小冰开源项目新闻客户端 */
+	public static final String GIS_OSCHINA_DAVIDTGNEWSPROJECT = "https://git.oschina.net/MCXIAOBING/DavidTGNewsProject";
+	/* 程序员小冰开源项目Android利器 */
+	public static final String GIT_OSCHINA_DAVID_ANDROID_PROJECT_TOOLS = "https://git.oschina.net/MCXIAOBING/DavidAndroidProjectTools";
+	/* 程序员小冰开源项目Android利器 */
+	public static final String GITHUB_DAVID_ANDROID_PROJECT_TOOLS = "https://github.com/QQ986945193/DavidAndroidProjectTools";
+	/* 程序员小冰OsChina空间 */
+	public static final String OSCHINA_DAVID = "https://my.oschina.net/mcxiaobing";
 }
