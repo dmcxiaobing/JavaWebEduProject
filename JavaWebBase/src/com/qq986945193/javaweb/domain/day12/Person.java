25a7eb8051d57ae51b25664fae7f8d4cad5d1eff
qq986945193 <986945193@qq.com>
2017-01-07 17:00:29 +0800

    commit javabean

---
+++ JavaWebBase/src/com/qq986945193/javaweb/domain/day12/Person.java
@@ -0,0 +1,53 @@
+package com.qq986945193.javaweb.domain.day12;
+/**
+ * @Author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @OsChina空间: https://my.oschina.net/mcxiaobing
+ */
+/*
+ * JavaBean
+ * 必须要有get/set方法(只有一个也是可以的。),必须要有无参构造方法
+ * */
+public class Person {
+	private String name;
+	private int age;
+	private String gender;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Person(String name, int age, String gender) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+
+	public Person() {
+		super();
+	}
+
+}
