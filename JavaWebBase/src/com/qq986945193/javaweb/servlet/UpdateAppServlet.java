e9c1dcf214ddb35cbab94dbf3d8363f6eff4d101
DAVID <986945193@qq.com>
2017-05-14 20:32:22 +0800

    commit update

--- JavaWebBase/src/com/qq986945193/javaweb/servlet/UpdateAppServlet.java
+++ JavaWebBase/src/com/qq986945193/javaweb/servlet/UpdateAppServlet.java
@@ -1,5 +1,11 @@
 package com.qq986945193.javaweb.servlet;
-
+/**
+ * @Author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @OsChina空间: https://my.oschina.net/mcxiaobing
+ */
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
