a285ba8df3a4dd0b4d0e2cee3fbd6f5ff06ee10a
qq986945193 <986945193@qq.com>
2017-06-22 14:46:28 +0800

    commit utils

---
+++ JavaWebBase/src/com/qq986945193/javaweb/utils/RegexUtils.java
@@ -0,0 +1,32 @@
+package com.qq986945193.javaweb.utils;
+
+import java.util.regex.Pattern;
+
+public class RegexUtils {
+
+	/**
+	 * 验证Email
+	 * 
+	 * @param email
+	 *            email地址，格式：dmcxiaobing@gmail.com，xxx代表邮件服务商
+	 * @return 验证成功返回true，验证失败返回false
+	 */
+	public static boolean checkEmail(String email) {
+		String regex = "\\w+@\\w+\\.[a-z]+(\\.[a-z]+)?";
+		return Pattern.matches(regex, email);
+	}
+
+	/**
+	 * 验证身份证号码
+	 * 
+	 * @param idCard
+	 *            居民身份证号码15位或18位，最后一位可能是数字或字母
+	 * @return 验证成功返回true，验证失败返回false
+	 */
+
+	public static boolean checkIdCard(String idCard) {
+		String regex = "[1-9]\\d{13,16}[a-zA-Z0-9]{1}";
+		return Pattern.matches(regex, idCard);
+	}
+
+}
